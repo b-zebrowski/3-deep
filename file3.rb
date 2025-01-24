97748866e05d41df46817a981668734dab3dd28f
b-zebrowski <6879017+b-zebrowski@users.noreply.github.com>
2025-01-24 08:24:29 -0500

    Create file3.rb

---
+++ file3.rb
@@ -0,0 +1,10 @@
+# This is a single line comment, you should not see the link to https://github.com/accountnotexist/some-repo
+
+=begin
+Now lets check a multi-line comment, you should not see a link to https://github.com/accountnotexist/some-repo
+=end
+=begin Another multi-line comment, you should not see a link to https://github.com/accountnotexist/some-repo =end
+
+this line you should see pointing towards https://github.com/b-zebrowski/2-deep
+
+this line should not show up as it ties back to a specific tag https://github.com/b-zebrowski/2-deep@123456
