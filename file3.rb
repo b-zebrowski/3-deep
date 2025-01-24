251e5f160b60bd409368b4e6f06506ac971ccc5e
b-zebrowski <6879017+b-zebrowski@users.noreply.github.com>
2025-01-24 08:28:22 -0500

    Update file3.rb

--- file3.rb
+++ file3.rb
@@ -1,9 +1,9 @@
-# This is a single line comment, you should not see the link to https://github.com/accountnotexist/some-repo
+# This is a single line comment, you should not see the link to https://github.com/b-zebrowski/2-deep
 
 =begin
-Now lets check a multi-line comment, you should not see a link to https://github.com/accountnotexist/some-repo
+Now lets check a multi-line comment, you should not see a link to https://github.com/b-zebrowski/2-deep
 =end
-=begin Another multi-line comment, you should not see a link to https://github.com/accountnotexist/some-repo =end
+=begin Another multi-line comment, you should not see a link to https://github.com/b-zebrowski/2-deep =end
 
 this line you should see pointing towards https://github.com/b-zebrowski/2-deep
 
