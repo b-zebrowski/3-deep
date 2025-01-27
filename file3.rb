8bcb45d3b91c175de0465a1905658305bde066a9
b-zebrowski <6879017+b-zebrowski@users.noreply.github.com>
2025-01-27 09:37:36 -0500

    Update file3.rb
    using real code

--- file3.rb
+++ file3.rb
@@ -3,8 +3,30 @@
 =begin
 Now lets check a multi-line comment, you should not see a link to https://github.com/b-zebrowski/2-deep
 =end
-=begin Another multi-line comment, you should not see a link to https://github.com/b-zebrowski/2-deep =end
 
-this line you should see pointing towards https://github.com/b-zebrowski/2-deep
+require 'net/http'
+require 'uri'
 
-this line should not show up as it ties back to a specific tag https://github.com/b-zebrowski/2-deep@123456
+# Function to fetch the content of a URL
+def fetch_url(url)
+  uri = URI.parse(url)
+  response = Net::HTTP.get_response(uri) # Perform the GET request
+
+  if response.is_a?(Net::HTTPSuccess)
+    puts "Response Body:\n\n#{response.body}"
+  else
+    puts "Failed to fetch the URL. HTTP Status: #{response.code} - #{response.message}"
+  end
+end
+
+# Specify the URL
+url = "https://github.com/b-zebrowski/2-deep" # this line you should see pointing towards 2-deep
+
+puts "Fetching URL: #{url}\n\n"
+fetch_url(url)
+
+# Specify the URL
+url = "https://github.com/b-zebrowski/2-deep@123456" # this line should not show up as it ties back to a specific tag
+
+puts "Fetching URL: #{url}\n\n"
+fetch_url(url)
